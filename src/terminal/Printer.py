76daddb204f8eadf0a863ab0a6214a815a88d205
Jared <jmw535@cornell.edu>
2017-03-23 03:29:09 -0400

    fix some background printing in toplevel

--- src/terminal/Printer.py
+++ src/terminal/Printer.py
@@ -10,9 +10,11 @@ class Printer:
     def __init__(self):
         x = os.popen('stty size', 'r').read().split()
         if (len(x) > 0):
+            self.print_enabled = True 
             self.rows = x[0]
             self.columns = x[1]
         else:
+            self.print_enabled = False 
             self.rows = 1
             self.columns = 50
         self.columns = min([int(self.columns), 50])
@@ -27,14 +29,20 @@ class Printer:
         return (' '*5 + text)
 
     def print_heading(self, text):
+        if not self.print_enabled:
+            return
         print() # print an empty line before all prints
         print_text = self.printable_title(text)
         print(print_text)
 
     def print_sep(self):
+        if not self.print_enabled:
+            return
         print('-' * self.columns)
         print()
 
     def print_option(self, text):
+        if not self.print_enabled:
+            return
         print_text = self.printable_option(text)
         print(print_text)
