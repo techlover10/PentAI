9b4fba47a56633e5c5ed7ff185e4fdb7ba3f105d
Jared <jmw535@cornell.edu>
2017-03-24 11:40:45 -0400

    fix stupid bug preventing win detection

--- src/logic/Logic.py
+++ src/logic/Logic.py
@@ -19,33 +19,35 @@ def line_count(board, xcoord, ycoord, direction, player):
     if board.get_piece(xcoord, ycoord) != player:
         return 0
 
+    # Note: board is indexed with origin being the top left corner 
+    # with coordinates (0,0), toplevel gameplay is 1-adjusted
     if direction == 'l':
-        return 1 + line_count(board, xcoord-1, ycoord, direction, player)
-    if direction == 'r':
-        return 1 + line_count(board, xcoord+1, ycoord, direction, player)
-    if direction == 'u':
         return 1 + line_count(board, xcoord, ycoord-1, direction, player)
+    if direction == 'r':
+        return 1 + line_count(board, xcoord, ycoord+1, direction, player)
+    if direction == 'u':
+        return 1 + line_count(board, xcoord-1, ycoord, direction, player)
     if direction == 'd':
-        return 1 + line_count(board, xcoord-1, ycoord+1, direction, player)
+        return 1 + line_count(board, xcoord+1, ycoord, direction, player)
     if direction == 'ul':
         return 1 + line_count(board, xcoord-1, ycoord-1, direction, player)
     if direction == 'ur':
-        return 1 + line_count(board, xcoord+1, ycoord-1, direction, player)
-    if direction == 'dl':
         return 1 + line_count(board, xcoord-1, ycoord+1, direction, player)
+    if direction == 'dl':
+        return 1 + line_count(board, xcoord+1, ycoord-1, direction, player)
     if direction == 'dr':
         return 1 + line_count(board, xcoord+1, ycoord+1, direction, player)
 
 # Main function for checking for win
 def check_win(board, xcoord, ycoord, player):
     #print("checking win: " + str(xcoord) + ',' + str(ycoord))
-    if line_count(board, xcoord, ycoord, 'l', player) + line_count(board, xcoord, ycoord, 'r', player + 1) >= 5:
+    if line_count(board, xcoord, ycoord-1, 'l', player) + line_count(board, xcoord, ycoord+1, 'r', player) + 1 >= 5:
         return True
-    if line_count(board, xcoord, ycoord, 'dl', player) + line_count(board, xcoord, ycoord, 'ur', player + 1) >= 5:
+    if line_count(board, xcoord+1, ycoord-1, 'dl', player) + line_count(board, xcoord-1, ycoord+1, 'ur', player) + 1 >= 5:
         return True
-    if line_count(board, xcoord, ycoord, 'ul', player) + line_count(board, xcoord, ycoord, 'dr', player + 1) >= 5:
+    if line_count(board, xcoord-1, ycoord-1, 'ul', player) + line_count(board, xcoord+1, ycoord+1, 'dr', player) + 1 >= 5:
         return True
-    if line_count(board, xcoord, ycoord, 'u', player) + line_count(board, xcoord, ycoord, 'd', player + 1) >= 5:
+    if line_count(board, xcoord-1, ycoord, 'u', player) + line_count(board, xcoord+1, ycoord, 'd', player) + 1 >= 5:
         return True
 
     if (board.get_captures(player)) >= 5:
