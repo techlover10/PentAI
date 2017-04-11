8da080e63135f2bbcc7ce5e58f9a89b1880c9591
Jared <jmw535@cornell.edu>
2017-04-11 16:30:30 -0400

    update

--- src/agents/MinimaxAgent.py
+++ src/agents/MinimaxAgent.py
@@ -9,11 +9,11 @@ from logic.Logic import heuristic_count
 class MinimaxAgent:
     def __init__(self, pid):
         self.H_VALS = {
-                1: 1,
-                2: 2,
-                3: 3,
-                4: 4,
-                5: 5
+                1: 0,
+                2: 1,
+                3: 2,
+                4: 3,
+                5: 4
                 }
         self.xcounter = 0
         self.ycounter = 0
